98bd04efc9822221703c0519782c5ee1d2cc0be2
harshit <harshityadav95@gmail.com>
2017-01-09 03:00:59 +0530

    Added two Basic Classes in Model

--- confrence/app/models.py
+++ confrence/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Speaker(models.Model):
+    name=models.CharField(max_length=40)
+    title=models.CharField(max_length=50)
+    bio=models.CharField(max_length=10000)
+
+class Tracks(models.Model):
+    title=models.CharField(max_length=50)
+    description=models.TextField(max_length=1000)
+
+
+
+
