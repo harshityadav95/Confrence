f5bc0532b9f402a24f01b904e771ceab269595f4
Harshit Yadav <harshityadav95@gmail.com>
2017-01-09 22:18:42 +0530

    Adding Multiple Checkbox Action and Queryset

--- confrence/app/admin.py
+++ confrence/app/admin.py
@@ -39,9 +39,28 @@ admin.site.register(Speaker,SpeakerAdmin)
 
 
 class SessionAdmin(admin.ModelAdmin):
-    list_display=('title','speaker')
+    list_display=('title','status')
     search_fields=['title','abstract']   
     list_filter=('track__title','speaker')
+    
+    # Calling of the Actions Mark Approved Defination 
+    actions=['make_approved',]
+
+    # function to perform action on selected fields  
+    def make_approved(self,request,queryset):
+        rows_updated=queryset.update(status='a')
+        if rows_updated==1:
+            message_bit='1 session was'
+        else:
+            message_bit='%s sessions were'%rows_updated
+
+            #Displaying User Error Message 
+        self.message_user(request,'%s approved.'%message_bit)
+
+        #Message to appear in the  Drop-Down  Actions Menu  
+    make_approved.short_description='Mark sessions(s) as approved'
+
+
 
 
 admin.site.register(Sessions,SessionAdmin)
--- confrence/app/models.py
+++ confrence/app/models.py
@@ -22,12 +22,21 @@ class Track(models.Model):
         return self.title
 
 
+SESSION_STATUSES=(
+        ('a','Approved'),
+        ('s','Submitted'),
+        ('r','Rejected'),
+)
+
+
+
 
 class Sessions(models.Model):
     title=models.CharField(max_length=50)
     abstract=models.CharField(max_length=2000)
     track=models.ForeignKey(Track)
     speaker=models.ForeignKey(Speaker)
+    status=models.CharField(max_length =1, choices=SESSION_STATUSES)
      # returning name in site 
     def __str__(self):
         return self.title
